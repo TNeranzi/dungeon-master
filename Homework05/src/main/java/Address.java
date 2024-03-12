30057d24d6f1e91ae63b3cd286a4e008ef743bf0
Teodora <160553040+TNeranzi@users.noreply.github.com>
2024-03-12 23:13:00 +0200

    Skeleton of the implementation. Classes, variables, getters, setters etc.

---
+++ Homework05/src/main/java/Address.java
@@ -0,0 +1,14 @@
+public class Address {
+
+    protected String street;
+    protected int number;
+    protected String building;
+    protected String room;
+
+    public Address(String street, int number, String building, String room) {
+        this.street = street;
+        this.number = number;
+        this.building = building;
+        this.room = room;
+    }
+}
